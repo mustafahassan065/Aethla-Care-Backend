21849f8222b7e4c2c5f4b34491f0aa50159150da
Mustafa <mustafaprogrammer786@gmail.com>
2026-06-26 17:33:58 +0500

    Fix the schema

--- src/modules/public/schemas/admin-signup.schema.ts
+++ src/modules/public/schemas/admin-signup.schema.ts
@@ -5,10 +5,10 @@ import { Document } from 'mongoose'
 export class AdminSignup extends Document {
   @Prop({ required: true }) firstName: string
   @Prop({ required: true }) lastName:  string
-  @Prop({ required: true, unique: true }) email: string
+  @Prop({ required: true }) email:     string
   @Prop({ required: true }) password:  string
-  @Prop({ required: true }) phone:     string
-  @Prop({ default: 'pending', enum: ['pending','approved','rejected'] }) status: string
+  @Prop({ default: ''    }) phone:     string
+  @Prop({ default: 'pending', enum: ['pending', 'approved', 'rejected'] }) status: string
 }
 
 export const AdminSignupSchema = SchemaFactory.createForClass(AdminSignup)
